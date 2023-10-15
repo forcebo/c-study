b77fb7bdf271fb12c88ec0b7b1196246d319028f
liuweibo <1974226194@qq.com>
2023-10-15 21:37:26 +0800

    指针的进阶(2)

--- test_10_12/test.c
+++ test_10_12/test.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <stdio.h>
-
+#include <stdlib.h>
 
 //int main()
 //{
@@ -237,19 +237,188 @@
 //	// 指向的数组的每个元素的类型是一个函数指针 int(*) (int, int)
 //}
 
-void print(char* str)
-{
-	printf("hehe:%s", str);
-}
+//void print(char* str)
+//{
+//	printf("hehe:%s", str);
+//}
+//
+//void test(void (*p)(char*))
+//{
+//	printf("test\n");
+//	p("bit");
+//}
+//
+//int main()
+//{
+//	test(print);
+//	return 0;
+//}
 
-void test(void (*p)(char*))
-{
-	printf("test\n");
-	p("bit");
-}
+////不够通用
+//void bubble_sort(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		int j = 0;
+//		int flag = 0;
+//		for (j = 0; j < sz - i - 1; j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				flag = 1;
+//				int temp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = temp;
+//			}
+//		}
+//		if (flag == 0)
+//		{
+//			break;
+//		}
+//	}
+//}
 
-int main()
-{
-	test(print);
-	return 0;
-}
+
+//typedef struct Stu
+//{
+//	char name[20];
+//	int age;
+//}Stu;
+//
+//// void qsort(void* base, size_t num, size_t width, int (*compare)(const void* elem1, const void* elem2));
+//
+//int cmp_int(const void* e1, const void* e2)
+//{
+//	// 比较两个整型值的大小
+//	return  *(int*)e1 - *(int*)e2;
+//}
+//
+//void test1()
+//{
+//	int arr[10] = { 9,8,7,6,5,4,3,2,1,0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	qsort(arr, sz, sizeof(arr[0]), cmp_int);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+//
+//int cmp_float(const void* e1, const void* e2)
+//{
+//	if (*(float*)e1 == *(float*)e2)
+//		return 0;
+//	else if (*(float*)e1 > *(float*)e2)
+//		return 1;
+//	else
+//		return -1;
+//}
+//
+//void test2()
+//{
+//	float f[] = { 9.0,8.0,7.0,6.0,5.0,4.0,3.0 };
+//	int sz = sizeof(f) / sizeof(f[0]);
+//	qsort(f, sz, sizeof(f[0]), cmp_float);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%f ", f[i]);
+//	}
+//	printf("\n");
+//}
+//
+//int cmp_stu_by_age(const void* e1, const void* e2)
+//{
+//	return ((Stu*)e1)->age - ((Stu*)e2)->age;
+//}
+//
+//int cmp_stu_by_name(const void* e1, const void* e2)
+//{
+//	return strcmp(((Stu*)e1)->name, ((Stu*)e1)->name);
+//}
+//
+//void test3()
+//{
+//	Stu s[3] = { {"zhangsan", 20}, {"lisi", 30}, {"wangwu", 10} };
+//	int sz = sizeof(s) / sizeof(s[0]);
+//
+//	qsort(s, sz, sizeof(s[0]), cmp_stu_by_name);
+//	// 第一个参数：待排序数组的首元素地址
+//	// 第二个参数：待排序的元素个数
+//	// 第三个参数：待排序数组的每个元素的大小 - 单位是字节
+//	// 第四个参数：是函数指针，比较两个元素的所用函数的地址-这个函数使用者自己实现
+//	// 函数指针的两个参数是：带比较的两个元素的地址
+//}
+//
+//void Swap(char* buf1, char* buf2, int width)
+//{
+//	int i = 0;
+//	for (i = 0; i < width; i++)
+//	{
+//		char tmp = *buf1;
+//		*buf1 = *buf2;
+//		*buf2 = tmp;
+//		buf1++;
+//		buf2++;
+//	}
+//}
+//
+//void bubble_sort(void* base, int sz, int width, int (*cmp)(const void* e1, const void* e2))
+//{
+//	int i = 0;
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		int flag = 0;
+//		int j = 0;
+//		for (j = 0; j < sz - i - 1; j++)
+//		{
+//			if (cmp((char*)base + j * width, (char*)base + (j + 1) * width) > 0)
+//			{
+//				// 交换
+//				Swap((char*)base + j * width, (char*)base + (j + 1) * width, width);
+//			}
+//		}
+//	}
+//}
+//
+//void test4()
+//{
+//	int arr[10] = { 9,8,7,6,5,4,3,2,1,0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	// 使用bubble_sort的程序员一定知道自己排序的是什么数据
+//	// 就应该知道如何比较待排序数组中的元素
+//	bubble_sort(arr, sz, sizeof(arr[0]),cmp_int);
+//}
+//
+//void test5()
+//{
+//	Stu s[3] = { {"zhangsan", 20}, {"lisi", 30}, {"wangwu", 10} };
+//	int sz = sizeof(s) / sizeof(s[0]);
+//
+//	bubble_sort(s, sz, sizeof(s[0]), cmp_stu_by_age);
+//}
+//
+//int main()
+//{
+//	//test1();
+//	//test2();
+//	//test3();
+//	//test4();
+//	test5();
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a;
+//	void* p = &a;
+//	// void* 类型的指针可以接收任意类型的地址
+//	// void* 类型的指针不能进行解引用操作
+//	// void* 类型的指针也不能进行+-整数的操作
+//	return 0;
+//}
+
