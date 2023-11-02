dfffdac6d92b49db8b9a1e817025dc42317e68a1
liuweibo <1974226194@qq.com>
2023-11-02 22:56:49 +0800

    通讯录（上）

--- 通讯录/contact.h
+++ 通讯录/contact.h
@@ -1,1 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+
+#define MAX 1000
+#define MAX_NAME 20
+#define MAX_SEX 5
+#define MAX_TELE 12
+#define MAX_ADDR 30
+
+
+
+typedef struct PeoInfo
+{
+	char name[MAX_NAME];
+	int age;
+	char sex[MAX_SEX];
+	char tele[MAX_TELE];
+	char addr[MAX_ADDR];
+}PeoInfo;
+
+typedef struct Contact
+{
+	PeoInfo data[MAX];
+	int size; // 记录当前已经有的元素个数
+}Contact;
+
+void InitContact(Contact* ps);
+void AddContact(Contact* ps);
+void ShowContact(const Contact* ps);
--- 通讯录/test.c
+++ 通讯录/test.c
@@ -1,1 +1,55 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include "contact.h"
+
+void menu()
+{
+	printf("*************************************\n");
+	printf("***** 1. add		2.del	  *******\n");
+	printf("***** 3. search		4.modify  *******\n");
+	printf("***** 5. show		6.sort    *******\n");
+	printf("***** 0. exit				  *******\n");
+	printf("*************************************\n");
+}
+
+
+int main()
+{
+	int input = 0;
+	int size = 0;
+	// 创建通讯录
+	Contact con;
+	InitContact(&con);
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			AddContact(&con);
+			break;
+		case 2:
+
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			ShowContact(&con);
+			break;
+		case 6:
+			break;
+		case 0:
+			printf("退出通讯录\n");
+			break;
+		default:
+			printf("选择错误\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
